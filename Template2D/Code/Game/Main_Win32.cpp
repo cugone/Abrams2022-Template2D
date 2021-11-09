7d122c1c3ef7014779a4299c2f715b4d5101a12a
cugone <cugone@gmail.com>
2021-11-08 23:51:59 -0600

    Initial commit

---
+++ Template2D/Code/Game/Main_Win32.cpp
@@ -0,0 +1,23 @@
+#include "Engine/Core/EngineBase.hpp"
+#include "Engine/Core/StringUtils.hpp"
+
+#include "Engine/Platform/Win.hpp"
+
+#include "Game/Game.hpp"
+
+#pragma warning(push)
+#pragma warning(disable: 28251)
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    UNUSED(hInstance);
+    UNUSED(hPrevInstance);
+    UNUSED(nCmdShow);
+    auto cmdString = StringUtils::ConvertUnicodeToMultiByte(std::wstring(pCmdLine ? pCmdLine : L""));
+    Engine<Game>::Initialize("Abrams 2022 2D Template", cmdString);
+    Engine<Game>::Run();
+    Engine<Game>::Shutdown();
+}
+
+#pragma warning(pop)
